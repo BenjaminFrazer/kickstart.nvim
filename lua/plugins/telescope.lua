e8e20be22611c1c9abe24340be0461f0099b1de2
benjamin frazer <benjamin.frazer@cabridgeaerospace.com>
2025-12-29 13:09:51 +0000

    <space> s G which does not respect git ignore

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -45,7 +45,9 @@ return {
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sf', function()
+      builtin.find_files { no_ignore = true, hidden = true }
+    end, { desc = '[S]earch all [F]iles (incl. ignored)' })
     vim.keymap.set('n', '<leader>ss', builtin.current_buffer_fuzzy_find, { desc = '[S]earch current buffer' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -54,22 +56,7 @@ return {
     vim.keymap.set('n', '<leader>si', builtin.lsp_document_symbols, { desc = '[S]earch Symbols' })
     vim.keymap.set('n', '<leader>,', builtin.oldfiles, { desc = 'Search Recent Files' })
     vim.keymap.set('n', '<leader>so', builtin.vim_options, { desc = '[S]earch [O]ptions' })
-    vim.keymap.set('n', '<leader><leader>', function()
-      -- Get git root directory
-      local git_root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
-      if vim.v.shell_error ~= 0 then
-        -- Not in a git repo, use current working directory
-        git_root = vim.fn.getcwd()
-      end
-      
-      -- Use oldfiles to get recently edited files, then filter by git root
-      builtin.oldfiles {
-        cwd = git_root,
-        cwd_only = true,
-        -- This will show only files from the current workspace
-        only_cwd = true,
-      }
-    end, { desc = 'Recent files in workspace' })
+    vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Find files (respects gitignore)' })
 
     -- Advanced keymaps
     vim.keymap.set('n', '<leader>/', function()

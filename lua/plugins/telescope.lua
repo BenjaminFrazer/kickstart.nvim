7cfe922f5a492404277c66f7d70b8e3f8f486995
benjamin frazer <benjamin.frazer@cabridgeaerospace.com>
2026-01-09 22:35:50 +0000

    Grep search added without git ignore (<space> s G)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -51,6 +51,11 @@ return {
     vim.keymap.set('n', '<leader>ss', builtin.current_buffer_fuzzy_find, { desc = '[S]earch current buffer' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sG', function()
+      builtin.live_grep {
+        additional_args = { '--no-ignore', '--hidden' },
+      }
+    end, { desc = '[S]earch by [G]rep (incl. ignored)' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>si', builtin.lsp_document_symbols, { desc = '[S]earch Symbols' })
